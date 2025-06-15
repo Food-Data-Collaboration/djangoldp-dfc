9e5672d1d3aeb0adca5396cb1f7f5e6574f822df
Calum Mackervoy <calummackervoy@protonmail.com>
2025-06-15 17:12:46 +0200

    Init Product models

--- data_food_consortium/models.py
+++ data_food_consortium/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from djangoldp.models import Model
 from djangoldp import fields
 
+from data_food_consortium.enums import ProductType
+
 
 class AbstractDFCModel(Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -149,6 +151,7 @@ class Enterprise(AbstractAgent):
             "data_server_source",
         ]
         nested_fields = ["addresses", "social_medias", "affiliates"]
+        disable_url = True  # Disables DjangoLDP auto-url generation
 
     def __str__(self):
         return self.urlid
@@ -268,3 +271,162 @@ class Person(AbstractAgent):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class AbstractProduct(AbstractDFCModel):
+    """
+    DFC Product ontology: https://lov.linkeddata.es/dataset/lov/vocabs/dfc-p
+    In terms of the ontology, an AbstractProduct is a DefinedProduct.
+
+    A DefinedProduct has variations depending on its relationship to the supply line:
+    - FunctionalProduct: represents the customers "need".
+    - TechnicalProduct: a distributor's proposal to satisfy a customer's need.
+    - SuppliedProduct: proposed by a producer to enable distributors to meet their promises to customers.
+        It is the implementation of a technical product.
+    - LocalizedProduct: a supplied product delivered to a specific locality.
+        Therefore, it is NOT a form of product, but rather a relationship between a locality and a product.
+    - LocalizedPhysicalProduct: a version of the localized product which has become physical.
+        e.g. a potato, once harvested.
+
+    A SuppliedProduct can differ from the TechnicalProduct for example in its implementation, and therefore each form is
+    not a "type" of product or a part of the life-cycle of the product, but is indeed a separate model. This is not
+    polymorphism.
+
+    In the current version of this package, only SuppliedProducts and LocalizedProducts are implemented and stored,
+    but the implementation of other Products is envisaged.
+    """
+
+    name = fields.TextField(
+        rdf_type="https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#name",
+        blank=True,
+        null=True,
+    )  # xsd:String
+    description = fields.TextField(
+        rdf_type="https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#description",
+        blank=True,
+        null=True,
+    )
+    has_type = fields.CharField(
+        rdf_type="https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#hasType",
+        verbose_name="has_type",
+        choices=ProductType.choices,
+        blank=True,
+        null=True,
+    )
+    url = fields.LDPUrlField(
+        rdf_type="https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#URL",
+        max_length=255,
+        blank=True,
+        null=True,
+    )  # xsd:anyURI
+
+    # TODO: Currently no serialization of reverse in the relationship. Wanted to avoid polymorphism if possible, but a product can be a
+    # variant of a product, or a product group, for example
+    # https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#hasVariant
+    is_variant_of = fields.LDPUrlField(
+        rdf_type="https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#isVariantOf",
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+
+    # Image
+    # has_percentage_of_alcohol_by_volume
+
+    # has_unit
+    # referenced_by: CatalogItem
+    # concerned_by: OrderLine
+    # consumed_by: AsPlannedConsumptionFlow
+    # has_process: Process
+    # lifetime: literal
+    # composes:
+    # has_characteristic: PhysicalCharacteristic
+    # hasAllergenCharacteristic: PhysicalCharacteristic
+    # has_nutrient_characteristic: PhysicalCharacteristic
+    # has_ingredient:
+    # has_brand: Brand
+    # brand: Brand
+    # has_certification: Label
+    # has_claim: Claim
+    # claim: Claim
+    # has_container_information:
+    # has_geographical_origin: GlobalGenericOrigin
+    # has_nature_origin: NatureOrigin
+    # has_part_origin: PartOrigin
+    # has_labelling_characteristic
+
+    class Meta:
+        abstract = True
+        rdf_type = "https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#DefinedProduct"
+
+
+class SuppliedProduct(AbstractProduct):
+    """
+    A supplied product is proposed by a producer to enable distributors to meet their promises to customers.
+    It is the implementation of a technical product.
+    """
+
+    supplied_by = fields.ForeignKey(
+        Enterprise,
+        rdf_type="https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#suppliedBy",
+        related_rdf_type="https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#supplies",
+        blank=True,
+        null=True,
+        related_name="supplies",
+        on_delete=models.SET_NULL,
+    )
+
+    # hasQuantity: dfc-b:QuantitativeValue
+    # produces: AsPlannedTransformation (via TechnicalProduct)
+
+    class Meta(AbstractProduct.Meta):
+        rdf_type = "https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#SuppliedProduct"
+        serializer_fields = [
+            "@id",
+            "name",
+            "description",
+            "has_type",
+            "url",
+            "is_variant_of",
+            "supplied_by",
+        ]
+        disable_url = True  # Disables DjangoLDP auto-url generation
+
+    def __str__(self):
+        if self.name is not None and len(self.name):
+            return f"{self.name} - {self.has_type}"
+        return self.urlid
+
+
+class SuppliedProductGroup(SuppliedProduct):
+    """
+    Is-a SuppliedProduct, but stored and retrieved separately due to being intended as a collection of SuppliedProducts.
+    """
+
+    pass
+
+
+class LocalizedProduct(AbstractDFCModel):
+    """
+    A localized product is a supplied product delivered to a specific locality.
+    Therefore, it is NOT a form of product, but rather a relationship between a locality and a product.
+    """
+
+    reference_of = fields.ForeignKey(
+        SuppliedProduct,
+        related_rdf_type="https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#hasReference",
+        rdf_type="https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#referenceOf",
+        blank=True,
+        null=True,
+        related_name="localized_products",
+        on_delete=models.RESTRICT,
+    )
+    # TODO: foreign key to locality
+
+    class Meta(AbstractDFCModel.Meta):
+        rdf_type = "https://github.com/datafoodconsortium/ontology/releases/latest/download/DFC_BusinessOntology.owl#LocalizedProduct"
+        serializer_fields = ["@id", "reference_of"]
+        disable_url = True  # Disables DjangoLDP auto-url generation
+
+    def __str__(self):
+        return f"Localized {self.reference_of}"
