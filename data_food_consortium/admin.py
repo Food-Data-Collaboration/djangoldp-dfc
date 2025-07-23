987900e9faa85e2a087c95e3a470dbbc00ccd7b6
Calum Mackervoy <calummackervoy@protonmail.com>
2025-07-23 18:24:04 +0200

    feat: register custom admin classes for each model

---
+++ data_food_consortium/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from djangoldp.admin import DjangoLDPAdmin
+
+from data_food_consortium import models
+
+
+class DFCModelAdmin(DjangoLDPAdmin):
+    list_display = ["urlid", "proxy_of"]
+
+
+@admin.register(models.Enterprise)
+class EnterpriseAdmin(DFCModelAdmin):
+    pass
+
+
+@admin.register(models.EnterpriseAddress)
+class EnterpriseAddressAdmin(DFCModelAdmin):
+    pass
+
+
+@admin.register(models.SocialMedia)
+class SocialMediaAdmin(DFCModelAdmin):
+    pass
+
+
+@admin.register(models.Person)
+class PersonAdmin(DFCModelAdmin):
+    pass
+
+
+@admin.register(models.SuppliedProduct)
+class SuppliedProductAdmin(DFCModelAdmin):
+    pass
