24b15608cda504764d246ff591cc27925f63f490
Calum Mackervoy <calummackervoy@protonmail.com>
2025-08-24 17:14:24 +0200

    feature(admin): Add search fields to admin classes

--- data_food_consortium/admin.py
+++ data_food_consortium/admin.py
@@ -6,28 +6,63 @@ from data_food_consortium import models
 
 class DFCModelAdmin(DjangoLDPAdmin):
     list_display = ["urlid", "proxy_of"]
+    search_fields = ["urlid", "proxy_of"]
 
 
 @admin.register(models.Enterprise)
 class EnterpriseAdmin(DFCModelAdmin):
-    pass
+    search_fields = ["urlid", "proxy_of", "name"]
 
 
 @admin.register(models.EnterpriseAddress)
 class EnterpriseAddressAdmin(DFCModelAdmin):
-    pass
+    search_fields = [
+        "urlid",
+        "proxy_of",
+        "city",
+        "country",
+        "postcode",
+        "region",
+        "street",
+        "address_of__urlid",
+        "address_of__name",
+        "address_of__proxy_of",
+    ]
 
 
 @admin.register(models.SocialMedia)
 class SocialMediaAdmin(DFCModelAdmin):
-    pass
+    search_fields = [
+        "urlid",
+        "proxy_of",
+        "url",
+        "enterprise__urlid",
+        "enterprise__name",
+        "enterprise__proxy_of",
+    ]
 
 
 @admin.register(models.Person)
 class PersonAdmin(DFCModelAdmin):
-    pass
+    search_fields = [
+        "first_name",
+        "last_name",
+        "proxy_of",
+        "urlid",
+        "email",
+        "phone_number",
+    ]
 
 
 @admin.register(models.SuppliedProduct)
 class SuppliedProductAdmin(DFCModelAdmin):
+    search_fields = [
+        "urlid",
+        "proxy_of",
+        "name",
+        "has_type",
+        "supplied_by__name",
+        "supplied_by__urlid",
+        "supplied_by__proxy_of",
+    ]
     list_display = ["urlid", "proxy_of", "name", "has_type", "supplied_by"]
